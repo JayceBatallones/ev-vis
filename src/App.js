d5e0c4f256367ef63f12d15a0096c6a33def2316
JayceBatallones <48848817+JayceBatallones@users.noreply.github.com>
2023-10-11 14:17:13 +1100

    (setup) EV dataset

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import V1 from './components/V1';
+import V2 from './components/V2';
+import V3 from './components/V3';
+import V4 from './components/V4';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <V1 />
+      <V2 />
+      <V3 />
+      <V4 />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/V1.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const V1 = () => {
+  return (
+    <div name="v1" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500 text-center'>V1</p>
+            </div>
+            <p className='text-xl mt-20'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique vero fugit consequuntur voluptatum animi deserunt necessitatibus nobis modi soluta aperiam adipisci labore blanditiis minima est, nulla beatae accusantium. Deleniti, ea!
+            </p>
+            <br />
+            <p className='text-xl'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore provident sapiente repudiandae ullam, ratione minus veniam molestiae sed reiciendis, ex ipsam necessitatibus totam incidunt voluptate explicabo, expedita temporibus quam quasi.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default V1
